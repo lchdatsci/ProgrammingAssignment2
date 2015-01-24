b9fcf1ee8ecf5fe08fafcd218f53fc206a791da5
lchdatsci <lchdatsci@users.noreply.github.com>
2015-01-24 13:41:10 -0800

    Submission for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,24 @@
 
 ## Write a short comment describing this function
 
+## This function stores a matrix object and a single
+## cached value.
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Generic value that you want to cache
+  cache <- NULL
+  ## Setter. Sets argument to 'x'. Clear cache
+  set <- function(y) {
+          x <<- y
+          ## Clear cache
+          cache <<- NULL
+  }
+  ## Getter
+  get <- function() x
+  setcache <- function(val) cache <<- val
+  getcache <- function() cache
+  list(set = set, get = get,
+       setcache = setcache,
+       getcache = getcache)
 }
 
 
@@ -12,4 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getcache()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setcache(inv)
+        inv
 }
+
